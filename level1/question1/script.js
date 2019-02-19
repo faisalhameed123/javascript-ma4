def456bae8afc4621cdaa7867db45f3cf26f21d9
faisalhameed123 <44834125+faisalhameed123@users.noreply.github.com>
2019-02-19 14:18:23 +0100

    Update script.js

---
+++ level1/question1/script.js
@@ -0,0 +1,11 @@
+<script type="text/javascript">
+    
+   
+document.getElementById("button1").addEventListener("click",button);
+    
+   
+    function button(){
+        alert("SUCCESS");
+    }
+      
+      </script>
